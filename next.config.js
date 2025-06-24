46110c9361edab710418a677c4d9c03626e65488
Community Assessment Developer <user@example.com>
2025-06-24 16:28:52 +0800

    Add GitHub Pages deployment support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    domains: ['restapi.amap.com', 'webapi.amap.com'],
+  },
   experimental: {
     appDir: true,
-  },
-  images: {
-    domains: ['restapi.amap.com', 'webapi.amap.com'],
   },
 }
 
